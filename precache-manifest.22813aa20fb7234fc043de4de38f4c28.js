8e18e9656a71c5b4769ac8c4745e2f682531ed03
Arven Aguinaldo <arvencaguinaldo@gmail.com>
2020-01-15 16:31:44 +0800

    Updates

---
+++ precache-manifest.22813aa20fb7234fc043de4de38f4c28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9883f28569e0e61f70a0c6cd5868f8be",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "a2decdfa396aa24d11a0",
+    "url": "/dashboard/static/css/2.3b19379d.chunk.css"
+  },
+  {
+    "revision": "794224fd517e512385d5",
+    "url": "/dashboard/static/css/main.6e65ae83.chunk.css"
+  },
+  {
+    "revision": "a2decdfa396aa24d11a0",
+    "url": "/dashboard/static/js/2.f1424aaa.chunk.js"
+  },
+  {
+    "revision": "a34ff0125cab7c32bc50ba794786f65d",
+    "url": "/dashboard/static/js/2.f1424aaa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "794224fd517e512385d5",
+    "url": "/dashboard/static/js/main.92aae8d9.chunk.js"
+  },
+  {
+    "revision": "f006959208ad3515cfc1",
+    "url": "/dashboard/static/js/runtime-main.1da6c821.js"
+  }
+]);
